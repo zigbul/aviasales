94f65b97f035eb7b4a3d8411d5834c8b33c3462f
Zigbul <parfenov.georgiy@bk.ru>
2021-02-01 16:18:54 +0300

    добавил reedux-thunk и redux devtools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducer';
 import App from './App';
 
-const loggerMiddleware = (store) => (next) => (action) =>{
-  const result = next(action);
-  console.log('Middleware', store.getState());
-  return result;
-};
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
 
-const store = createStore(reducer, applyMiddleware(loggerMiddleware));
+// const loggerMiddleware = (store) => (next) => (action) =>{
+//   const result = next(action);
+//   console.log('Middleware', store.getState());
+//   return result;
+// };
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
